26417caabc10e39061354d02c60852be4c681fdb
GiddyLinux <86152883+GiddyLinux@users.noreply.github.com>
2022-01-13 11:36:07 +0000

    Delete setup.py

--- setup.py
+++
@@ -1,5 +0,0 @@
-import os
-os.system('pip install -r requirements.txt')
-os.system('sudo chmod +x ipscanner.py')
-os.system('sudo mv ipscanner.py ipscanner')
-os.system('sudo mv ipscanner /usr/bin')
