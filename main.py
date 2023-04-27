ee47e00c4a7772d9ed9165f162926c07709c4e03
meow <30691849+1839b@users.noreply.github.com>
2023-04-27 13:11:14 +0100

    swag

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ def main():
                 print_result(ip,port,2,hostname)
             case _:
                 print_result(ip,port,1,hostname)
+
 # sets the switches for the ping command based on the current os
 def os_settings():
     global count_packets
@@ -42,6 +43,7 @@ def os_settings():
     elif os.name == 'posix':
         count_packets = "-c"
         packet_timeout = "-W"
+
 # generate IP
 def generate_ip():
     ip = []
@@ -50,11 +52,13 @@ def generate_ip():
         ip.append(str(random.randint(0,255)))
     # joins the list together with .'s
     return '.'.join(ip)
+
 # check if IP is up
 def check_ip(ip):
     # creates a new subprocess the pings the IP address with 1 packet with a timeout of 1 second
     up = subprocess.run(['ping', packet_timeout, '1', count_packets, '1', ip],stdout=subprocess.DEVNULL)
     return up.returncode
+
 # scan IP
 def scan_ip(ip, port):
     # creates new socket
@@ -67,9 +71,11 @@ def scan_ip(ip, port):
     except socket.error:
         print("Couldn't connect to %s:%d" % (ip, port))
     return checkForPort
+
 # get current time
 def get_time():
     return datetime.now().strftime('%H:%M:%S')
+
 # get the hostname from the IP address
 def get_hostname(ip):
     global hostname
@@ -77,6 +83,7 @@ def get_hostname(ip):
         hostname = socket.gethostbyaddr(ip)[0]
     except socket.herror:
         return
+
 # prints the resuts out
 def print_result(ip,port,result,hostname):
     match result:
