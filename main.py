cc8b20673cbabfe854f1cda5bbb05846e96e355a
pwn4d <86152883+pwn4d@users.noreply.github.com>
2023-04-28 21:28:22 +0000

    NOT WORKING YET

--- main.py
+++ main.py
@@ -3,11 +3,98 @@ import socket
 import subprocess
 import os
 from datetime import datetime 
+from urllib.request import urlopen  
+import urllib
+import re
+import ssl
 
 # variables that dont need to be changed
 count_packets = str()
 packet_timeout = str()
 port = 80
+matches = []
+
+web_services = {
+
+    "Adobe Experience Manager": ["Adobe Experience Manager", "AEM"],
+    "Akamai": ["Akamai"],
+    "Alibaba Cloud": ["Alibaba Cloud"],
+    "Amazon Web Services": ["Amazon Web Services", "AWS"],
+    "Apache": ["Apache", "httpd"],
+    "ASP.NET": ["ASP.NET"],
+    "Atlassian": ["Atlassian"],
+    "Bluehost": ["Bluehost"],
+    "Bootstrap": ["Bootstrap"],
+    "cPanel": ["cPanel"],
+    "Cloudflare": ["Cloudflare"],
+    "Comodo": ["Comodo"],
+    "cPanel": ["cPanel"],
+    "Drupal": ["Drupal"],
+    "Dynatrace": ["Dynatrace"],
+    "F5 BIG-IP": ["F5 BIG-IP"],
+    "Facebook": ["Facebook"],
+    "Fastly": ["Fastly"],
+    "Firebase": ["Firebase"],
+    "GitLab": ["GitLab"],
+    "GitHub": ["GitHub"],
+    "Gmail": ["Gmail"],
+    "GoDaddy": ["GoDaddy"],
+    "Google Ads": ["Google Ads", "AdWords"],
+    "Google Analytics": ["Google Analytics"],
+    "Google Cloud": ["Google Cloud"],
+    "Google Maps": ["Google Maps"],
+    "Google Tag Manager": ["Google Tag Manager"],
+    "Heroku": ["Heroku"],
+    "Hikvision": ["Hikvision"],
+    "IBM Cloud": ["IBM Cloud"],
+    "Joomla": ["Joomla"],
+    "jQuery": ["jQuery"],
+    "Jetty": ["Jetty"],
+    "Kubernetes": ["Kubernetes"],
+    "Let's Encrypt": ["Let's Encrypt"],
+    "Lighttpd": ["Lighttpd"],
+    "Magento": ["Magento"],
+    "Mailchimp": ["Mailchimp"],
+    "MaxCDN": ["MaxCDN"],
+    "Microsoft Azure": ["Microsoft Azure"],
+    "Microsoft IIS": ["Microsoft IIS"],
+    "Nginx": ["Nginx"],
+    "Node.js": ["Node.js"],
+    "Oracle Cloud": ["Oracle Cloud"],
+    "Plesk": ["Plesk"],
+    "Python": ["Python"],
+    "Qualys": ["Qualys"],
+    "Rackspace": ["Rackspace"],
+    "React": ["React"],
+    "Red Hat": ["Red Hat"],
+    "Salesforce": ["Salesforce"],
+    "SAP": ["SAP"],
+    "Shopify": ["Shopify"],
+    "Sitecore": ["Sitecore"],
+    "Squarespace": ["Squarespace"],
+    "SSL.com": ["SSL.com"],
+    "Stripe": ["Stripe"],
+    "Sucuri": ["Sucuri"],
+    "Tawk.to": ["Tawk.to"],
+    "TeamViewer": ["TeamViewer"],
+    "Tencent Cloud": ["Tencent Cloud"],
+    "Tomcat": ["Tomcat"],
+    "Travis CI": ["Travis CI"],
+    "Twilio": ["Twilio"],
+    "Twitter": ["Twitter"],
+    "Ubuntu": ["Ubuntu"],
+    "Varnish": ["Varnish"],
+    "Vimeo": ["Vimeo"],
+    "VMware": ["VMware"],
+    "W3 Total Cache": ["W3 Total Cache"],
+    "Wix": ["Wix"],
+    "Weebly": ["Weebly"],
+    "WHM": ["WHM"],
+    "Windows Server": ["Windows Server"],
+    "WordPress": ["WordPress"],
+    "XenForo": ["XenForo"],
+    "Yahoo!": ["Yahoo!"]}
+
 
 def main():
     os_settings()
@@ -24,14 +111,16 @@ def main():
             case 0: 
                 scan = scan_ip(ip,port)
             case 1:
-                print_result(ip,port,0,hostname)
+                print_result(ip,port,0,hostname,matches)
                 continue
         match scan:
             case 0:
                 get_hostname(ip)
-                print_result(ip,port,2,hostname)
+                fingerprint(ip)
+                print_result(ip,port,2,hostname,matches)
+
             case _:
-                print_result(ip,port,1,hostname)
+                print_result(ip,port,1,hostname,matches)
 
 # sets the switches for the ping command based on the current os
 def os_settings():
@@ -85,14 +174,36 @@ def get_hostname(ip):
         return
 
 # prints the resuts out
-def print_result(ip,port,result,hostname):
+def print_result(ip,port,result,hostname, matches):
     match result:
         case 0:
-            print(f"\r{get_time()} - {ip} is not a valid IP address \033[K ", end="\r")
+            print(f"\r{get_time()} - {ip} is not a valid IP address  ", end="\r")
         case 1:
-            print(f"\r{get_time()} - {ip}:{port}  is not open \033[K ", end="\r")
+            print(f"\r{get_time()} - {ip}:{port}  is not open  ", end="\r")
         case 2:
-            print(f"\033[K{get_time()} - {ip}:{port} ({hostname}) is open")
+            print(f"\033[K{get_time()} - {ip}:{port} ({hostname}) is open ({', '.join(matches)})")
+
+# attempt to fingerprint what type of service is running
+def fingerprint(ip):
+    global matches
+    if port == 80 or port == 443:
+        try:
+            website = urlopen(f"https://{ip}:{port}").read().decode("utf8")
+        except (ssl.SSLError,urllib.error.URLError):
+            matches = ["Could not fingerprint services, if you know what service is running please make a pull request at https://github.com/pwn4d/Ipscanner"]
+            return
+        for service, keywords in web_services.items():
+         
+            for keyword in keywords:
+                matches += re.findall(keyword, website, re.IGNORECASE)
+            if matches:
+                print(matches)
+            else:
+                matches = ["Could not fingerprint services, if you know what service is running please make a pull request at https://github.com/pwn4d/Ipscanner"]
+                print(matches)
+
+
+
 
 
 main()
